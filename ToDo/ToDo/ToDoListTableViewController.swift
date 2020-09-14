c2c79d352876b2f1b47540956669670698b89d1f
Enrique Gongora <EGTech24@gmail.com>
2020-09-13 19:50:12 -0700

    Initial Commit

---
+++ ToDo/ToDo/ToDoListTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListTableViewController.swift
+//  ToDo
+//
+//  Created by Enrique Gongora on 9/13/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
